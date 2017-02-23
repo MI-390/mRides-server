92cfb34cc6bbf3630427e51aba91223e8cf07283
nim442 <nim442@gmail.com>
2017-02-23 15:15:30 -0500

    Updated matching session

---
+++ src/mRides-server/Controllers/ConsoleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mRides_server.Data;
+using mRides_server.Models;
+using Microsoft.EntityFrameworkCore;
+using mRides_server.Logic;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mRides_server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+
+    public class ConsoleController : Controller
+    {
+        private ServerContext _context;
+        private MatchingSession _matchingSession;
+        public ConsoleController(ServerContext context)
+        {
+            _context = context;
+            _matchingSession = new MatchingSession(context);
+        }
+
+
+        
+  
+       
+
+    }
+}
---
+++ src/mRides-server/Logic/MatchingSession.cs
@@ -0,0 +1,17 @@
+﻿using mRides_server.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mRides_server.Logic
+{
+    public class MatchingSession
+    {
+        ServerContext _context;
+        public MatchingSession(ServerContext context)
+        {
+            _context = context;
+        }
+    }
+}
