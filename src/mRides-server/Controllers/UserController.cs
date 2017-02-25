380e0b1bf2411d39193801b7a2957293ed126ea5
nim442 <nim442@gmail.com>
2017-02-24 21:06:18 -0500

    rrrr

--- src/mRides-server/Controllers/UserController.cs
+++ src/mRides-server/Controllers/UserController.cs
@@ -31,7 +31,7 @@ namespace mRides_server.Controllers
         [HttpGet("{id}")]
         public object getUserByFacebookId(int id)
         {
-            return _context.Users.First(u=>u.facebookID==id);
+            return _context.Users.SingleOrDefault(u=>u.facebookID==id);
         }
         [HttpGet("{id}")]
         public object getUser(int id)
