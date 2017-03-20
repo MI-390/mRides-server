3b6935077605a00cda5ffce23e1a60b7bf5ab831
nim442 <nim442@gmail.com>
2017-03-19 21:24:07 -0400

    DestCoordinate did not have getters setters therefore inaccesible

---
+++ src/mRides-server/Migrations/20170320011358_updatedDestCoord5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mRidesserver.Migrations
+{
+    public partial class updatedDestCoord5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
