ac29969c62290d7de9a7ae8fd72f07c4156beb35
Aline K <aline.koftikian@gmail.com>
2017-04-02 22:42:57 -0400

    Removed uncessesary inline comments from UserController class.
    These comments are redundant and confusing as they are past code therefore they are removed to keep the code more concise.

--- src/mRides-server/Controllers/UserController.cs
+++ src/mRides-server/Controllers/UserController.cs
@@ -19,6 +19,7 @@ namespace mRides_server.Controllers
     {
 
         private UserCatalog _userCatalog;
+
         public UserController(ICatalog<User> userCatalog)
         {
             _userCatalog = (UserCatalog)userCatalog;
@@ -42,11 +43,13 @@ namespace mRides_server.Controllers
         {
             return _userCatalog.get(id);
         }
+
         [HttpGet("{id}")]
         public virtual ICollection<Request> getRequests(int id)
         {
             return _userCatalog.getRequests(id);
         }
+
         [HttpPost]
         public Boolean updateFcmToken([FromHeader]string id, [FromBody]dynamic sentObj)
         {
@@ -54,8 +57,9 @@ namespace mRides_server.Controllers
            return _userCatalog.updateFcmToken(userId, (string)sentObj.fcmToken);
 
         }
+
         /// <summary>
-        /// This class is used as an api for the search requests.
+        /// This method is used as an api for the search requests.
         /// </summary>
         // POST api/values
         [HttpPost]
@@ -100,8 +104,6 @@ namespace mRides_server.Controllers
         [HttpPost]
         public long setGSD([FromBody]dynamic sentObject)
         {
-            //int newUserId = Convert.ToInt32(userId);
-            //long newAmountGSD = long.Parse(sentObject.amountGSD);
             int newUserId = sentObject.userId;
             int newAmountGSD = sentObject.amountGSD;
             _userCatalog.setGSD(newUserId, newAmountGSD);
