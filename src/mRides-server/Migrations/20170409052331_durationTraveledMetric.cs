10ba1eaad64f655e1cf4cc7e25d4eb87b1b47388
Aline K <aline.koftikian@gmail.com>
2017-04-09 01:35:46 -0400

    MI-207 Added duration of travel metric to db
    Added duration of travel metric to the database, Ride table. It will be stored as a long attribute of the duration of the ride in seconds.
    Also added the respective code to update this value in the database in the ride controller and ride catalog, with their respective documentation (XML comments).

---
+++ src/mRides-server/Migrations/20170409052331_durationTraveledMetric.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mRidesserver.Migrations
+{
+    public partial class durationTraveledMetric : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "duration",
+                table: "Ride",
+                nullable: true,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "duration",
+                table: "Ride");
+        }
+    }
+}
